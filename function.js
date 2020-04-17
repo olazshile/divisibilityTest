18572bcca0a1a89e76b9e61cb4e38276f4634e55
olazshile <olashilequadry@gmail.com>
2020-04-17 23:29:06 +0100

    adding existing files

---
+++ function.js
@@ -0,0 +1,57 @@
+
+// function to check for number divisibility 
+var number;
+
+    function divisibilityTest(number){
+
+        var new_array = [];
+
+        // get all the numbers starting from 1 until last digit
+        for (var index = 1; index <= number; index++) {
+
+                new_array.push(index);
+                
+            // check if number is divisible by 2
+            if(index % 2 == 0) {
+                new_array.pop();
+                new_array.push('yu');
+            }
+            // check if number is divisible by 3 
+            if(index % 3 == 0) {
+                new_array.pop();
+                new_array.push('gi');
+            }
+            // check if number is divisible by 5 
+            if(index % 5 == 0) {
+                new_array.pop();
+                new_array.push('oh');
+            }
+            // check if number is divisible by 2 and 3
+            if(index % 2 == 0 && index % 3 == 0) {
+                new_array.pop();
+                new_array.push('yu-gi');
+            }       
+            // check if number is divisible by 2 and 5 
+            if(index % 2 == 0 && index % 5 == 0) {
+                new_array.pop();
+                new_array.push('yu-oh');
+            }
+            // check if number is divisible by 3 and 5 
+            if(index % 3 == 0 && index % 5 == 0) {
+                    new_array.pop();
+                    new_array.push('gi-oh');
+            }
+            // check if number is divisible by 2,3 and 5 
+            if(index % 2 == 0 && index % 3 == 0 && index % 5 == 0) {
+                new_array.pop();
+                new_array.push('yu-gi-oh');
+            }
+            // ============== End of Divisibility Test =================
+    
+        }
+            return new_array;
+
+    }
+
+        console.log(divisibilityTest(100));
+        console.log(divisibilityTest(30));
