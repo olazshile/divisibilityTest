98702d4243df37e59a8b3a4d1a1578b46262cb68
Olashile Quadri <62192529+olazshile@users.noreply.github.com>
2020-04-19 16:53:43 +0100

    added new changes to file

--- function.js
+++ function.js
@@ -4,37 +4,38 @@
     function divisibilityTest(number){
 
         var new_array = [];
+        var str = '-';
 
         // get all the numbers starting from 1 until last digit
         for (var index = 1; index <= number; index++) {
 
             //start from the biggest and counting downward
             if (index%2 == 0 && index%3 == 0 && index%5 == 0){ 
-                new_array.push("yu-gi-oh");
+                new_array.push(`yu${str}gi${str}oh`);
                 // check if number is divisible by 2,3 and 5 
             }else
             if (index%3 == 0 && index%5 == 0){ 
-                new_array.push("gi-oh");
+                new_array.push(`gi${str}oh`);
                 // check if number is divisible by 3 and 5
             }else 
             if (index%2 == 0 && index%5 == 0){ 
-                new_array.push("yu-oh");
+                new_array.push(`yu${str}oh`);
                 // check if number is divisible by 2 and 5 
             }else
             if (index%2 == 0 && index%3 == 0){ 
-                new_array.push("yu-gi");
+                new_array.push(`yu${str}gi`);
                 // check if number is divisible by 2 and 3 
             }else
             if (index%5 == 0){
-                new_array.push("oh");
+                new_array.push(`oh`);
                 // check if number is divisible by 5
             }else  
             if (index%3 == 0){
-                new_array.push("gi");
+                new_array.push(`gi`);
                 // check if number is divisible by 3
             }else  
             if (index%2 == 0){
-                new_array.push("yu");    
+                new_array.push(`yu`);    
                 // check if number is divisible by 3 
             }
             else {
@@ -44,10 +45,11 @@
             // ======= End of Divisibility Test =======
     
         }
+            console.log(new_array);
             return new_array;
 
     }
 
-        console.log(divisibilityTest(100));
-        console.log(divisibilityTest(30));
+            (divisibilityTest(100));
+            (divisibilityTest(30));
     
