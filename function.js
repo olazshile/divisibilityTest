b809be399b13e768aa4cc642716d20e61380c5b2
Olashile Quadri <62192529+olazshile@users.noreply.github.com>
2020-04-19 16:12:42 +0100

    new file added

--- function.js
+++ function.js
@@ -1,6 +1,5 @@
 
 // function to check for number divisibility 
-var number;
 
     function divisibilityTest(number){
 
@@ -9,44 +8,40 @@ var number;
         // get all the numbers starting from 1 until last digit
         for (var index = 1; index <= number; index++) {
 
+            //start from the biggest and counting downward
+            if (index%2 == 0 && index%3 == 0 && index%5 == 0){ 
+                new_array.push("yu-gi-oh");
+                // check if number is divisible by 2,3 and 5 
+            }else
+            if (index%3 == 0 && index%5 == 0){ 
+                new_array.push("gi-oh");
+                // check if number is divisible by 3 and 5
+            }else 
+            if (index%2 == 0 && index%5 == 0){ 
+                new_array.push("yu-oh");
+                // check if number is divisible by 2 and 5 
+            }else
+            if (index%2 == 0 && index%3 == 0){ 
+                new_array.push("yu-gi");
+                // check if number is divisible by 2 and 3 
+            }else
+            if (index%5 == 0){
+                new_array.push("oh");
+                // check if number is divisible by 5
+            }else  
+            if (index%3 == 0){
+                new_array.push("gi");
+                // check if number is divisible by 3
+            }else  
+            if (index%2 == 0){
+                new_array.push("yu");    
+                // check if number is divisible by 3 
+            }
+            else {
                 new_array.push(index);
-                
-            // check if number is divisible by 2
-            if(index % 2 == 0) {
-                new_array.pop();
-                new_array.push('yu');
             }
-            // check if number is divisible by 3 
-            if(index % 3 == 0) {
-                new_array.pop();
-                new_array.push('gi');
-            }
-            // check if number is divisible by 5 
-            if(index % 5 == 0) {
-                new_array.pop();
-                new_array.push('oh');
-            }
-            // check if number is divisible by 2 and 3
-            if(index % 2 == 0 && index % 3 == 0) {
-                new_array.pop();
-                new_array.push('yu-gi');
-            }       
-            // check if number is divisible by 2 and 5 
-            if(index % 2 == 0 && index % 5 == 0) {
-                new_array.pop();
-                new_array.push('yu-oh');
-            }
-            // check if number is divisible by 3 and 5 
-            if(index % 3 == 0 && index % 5 == 0) {
-                    new_array.pop();
-                    new_array.push('gi-oh');
-            }
-            // check if number is divisible by 2,3 and 5 
-            if(index % 2 == 0 && index % 3 == 0 && index % 5 == 0) {
-                new_array.pop();
-                new_array.push('yu-gi-oh');
-            }
-            // ============== End of Divisibility Test =================
+           
+            // ======= End of Divisibility Test =======
     
         }
             return new_array;
@@ -55,3 +50,4 @@ var number;
 
         console.log(divisibilityTest(100));
         console.log(divisibilityTest(30));
+    
